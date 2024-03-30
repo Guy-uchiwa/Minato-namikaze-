fb7771279cb32e4b79278c5b7cb76a68ef17a57d
Pain <162317216+Enockkouadi24@users.noreply.github.com>
2024-03-30 04:08:41 +0000

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -3,18 +3,23 @@ const fs = require("fs-extra");
 module.exports = {
 	config: {
 		name: "restart",
-		version: "1.1",
+		version: "1.0",
 		author: "NTKhang",
 		countDown: 5,
 		role: 2,
-		description: {
+		shortDescription: {
 			vi: "Khởi động lại bot",
-			en: "Restart bot"
+			en: "𝗣𝗹𝗼𝗻𝗴𝗲́ 𝗱𝗮𝗻𝘀 𝗹𝗲 𝗹𝗶𝗺𝗯𝗲 𝗹𝗲 𝗯𝗼𝘁 𝗿𝗲𝗱𝗲́𝗺𝗮𝗿𝗿𝗲"
+		},
+		longDescription: {
+			vi: "Khởi động lại bot",
+			en: "🖐| 𝗥𝗲́𝗽𝘂𝘁𝗮𝘁𝗶𝗼𝗻 𝗰𝗲́𝗹𝗲𝘀𝘁𝗲 𝗹𝗲 𝗯𝗼𝘁 𝗿𝗲𝗱𝗲́𝗺𝗮𝗿𝗿𝗲"
 		},
 		category: "Owner",
 		guide: {
 			vi: "   {pn}: Khởi động lại bot",
-			en: "   {pn}: Restart bot"
+			en: "   {pn}: 🖐 | 𝗥𝗲́𝗽𝘂𝘁𝗮𝘁𝗶𝗼𝗻 𝗰𝗲́𝗹𝗲𝘀𝘁𝗲 𝗹𝗲 𝗯𝗼𝘁 𝗿𝗲𝗱𝗲́𝗺𝗮𝗿𝗿𝗲",
+				
 		}
 	},
 
@@ -23,7 +28,7 @@ module.exports = {
 			restartting: "🔄 | Đang khởi động lại bot..."
 		},
 		en: {
-			restartting: "🔄 | Restarting bot..."
+			restartting: "🔌| ᖇᗴᗪᗴᗰᗩᖇᗩᘜᗴ ᗪᑌ ᗷOT ⚪🔵🔴 "
 		}
 	},
 
@@ -31,7 +36,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ |ᖇᗴᗪᗴᗰᗩᖇᗩᘜᗴ TᗴᖇᗰIᑎᗴ\n⏰ | TᗴᗰᑭՏ: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
